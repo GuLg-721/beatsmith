c5d2d84f548ff260b875ac3a6e42a956feca0034
GuLg-721 <1986269596@qq.com>
2026-06-06 19:27:00 +0800

    fix(theme): osu uses ParticleBackground, cyberpunk/valorant use their own backgrounds

--- client/src/components/common/ThemeBackground.vue
+++ client/src/components/common/ThemeBackground.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useAuthStore } from '@/stores/authStore'
-import OsuBackground from './OsuBackground.vue'
+import ParticleBackground from './ParticleBackground.vue'
 import CyberpunkBackground from './CyberpunkBackground.vue'
 import ValorantBackground from './ValorantBackground.vue'
 
@@ -13,7 +13,7 @@ const currentTheme = computed(() => authStore.user?.theme || 'osu')
 </script>
 
 <template>
-  <OsuBackground v-if="currentTheme === 'osu'" :reduced="reduced" />
+  <ParticleBackground v-if="currentTheme === 'osu'" />
   <CyberpunkBackground v-else-if="currentTheme === 'cyberpunk'" :reduced="reduced" />
   <ValorantBackground v-else-if="currentTheme === 'valorant'" :reduced="reduced" />
 </template>
