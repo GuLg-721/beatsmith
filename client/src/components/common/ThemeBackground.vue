ee3bafc0b21b994fe291186dc3e102932c0e75c3
GuLg-721 <1986269596@qq.com>
2026-06-06 18:54:37 +0800

    feat(background): add ThemeBackground wrapper component

---
+++ client/src/components/common/ThemeBackground.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
+import OsuBackground from './OsuBackground.vue'
+import CyberpunkBackground from './CyberpunkBackground.vue'
+import ValorantBackground from './ValorantBackground.vue'
+
+const props = defineProps<{ reduced?: boolean }>()
+
+const authStore = useAuthStore()
+
+const currentTheme = computed(() => authStore.user?.theme || 'osu')
+</script>
+
+<template>
+  <OsuBackground v-if="currentTheme === 'osu'" :reduced="reduced" />
+  <CyberpunkBackground v-else-if="currentTheme === 'cyberpunk'" :reduced="reduced" />
+  <ValorantBackground v-else-if="currentTheme === 'valorant'" :reduced="reduced" />
+</template>
